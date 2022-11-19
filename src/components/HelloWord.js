a1233ae53df793a588a2d52776f56db65554e8a0
4ngelo0 <gabrielb4rr00s@gmail.com>
2022-11-18 23:18:10 -0300

    componet/readme

---
+++ src/components/HelloWord.js
@@ -0,0 +1,13 @@
+
+// Geralmente a function tem o mesmo nome do arquivo criado
+function HelloWord(){
+
+    return(
+        <div>
+            <h1>Meu primeiro Componente</h1>
+        </div>
+    )
+}
+
+export default HelloWord;
+
