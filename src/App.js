73c95382d7032d9dcc86aecfef0257a0d8798155
4ngelo0 <gabrielb4rr00s@gmail.com>
2022-11-18 22:24:27 -0300

    JSX aprendendo e anotações do readme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+//Aqui será a area para importação de aplicativos
+
 import "./App.css";
 
 function App() {
+//Criando a lógica por tras do HTML
+
+const name = 'Gabriel';
+const newName = name.toUpperCase();
+
+function sum(a, b){
+  return a + b;
+}
+
+const url = "https://via.placeholder.com/150"
+
   return (
+
+    // sempre usar className em JSX ao inves de só class
     <div className="App">
-      <h1>Olá Mundo</h1>
-      <p>Meu primeiro projeto</p>
+
+      {/* posso puxar variaveis de fora do return */}
+      <p>Olá, {name}</p>
+
+      {/* consigo alterar e puxar novamente a variavel */}
+      <p>Olá, {newName}</p>
+
+      {/* é possível a realização de equações */}
+      <p>Soma: {sum(1, 2)}</p>
+
+      <img src={url} alt="Minha Imagem" />
     </div>
   )
 }
